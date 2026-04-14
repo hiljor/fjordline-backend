ed8902eeba96d42f7f7a405e60b5fd0fec22cef4
hiljor <hildejordal01@gmail.com>
2026-04-14 09:55:35 +0200

    missing T in ISO format data

--- src/data/seed.ts
+++ src/data/seed.ts
@@ -18,7 +18,7 @@ const initialDepartures: Departure[] = [
     maxVehicleCapacity: 500,
     legs: [
       { from: "Bergen", to: "Stavanger", departureTime: "2026-06-01T15:00:00Z", arrivalTime: "2026-06-01T20:00:00Z", occupiedPassengerCapacity: 0, occupiedVehicleCapacity: 0 },
-      { from: "Stavanger", to: "Hirtshals", departureTime: "2026-06-01T21:00:00Z", arrivalTime: "2026-06-0210:00:00Z", occupiedPassengerCapacity: 0, occupiedVehicleCapacity: 0 },
+      { from: "Stavanger", to: "Hirtshals", departureTime: "2026-06-01T21:00:00Z", arrivalTime: "2026-06-02T10:00:00Z", occupiedPassengerCapacity: 0, occupiedVehicleCapacity: 0 },
       { from: "Hirtshals", to: "Kristiansand", departureTime: "2026-06-02T12:00:00Z", arrivalTime: "2026-06-02T15:30:00Z", occupiedPassengerCapacity: 0, occupiedVehicleCapacity: 0 }
     ]
   },
